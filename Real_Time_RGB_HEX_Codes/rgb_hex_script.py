f63316dc8a0a8bb64f1a9807b58f57baf3139224
pabloescobarbell <johnathanarcay@gmail.com>
2026-02-17 16:31:34 +0000

    changed the keyboard listener to be a non-blocking type and moved out of while loop. Added append functionality to record values when specific key is pressed during program run time.

--- Real_Time_RGB_HEX_Codes/rgb_hex_script.py
+++ Real_Time_RGB_HEX_Codes/rgb_hex_script.py
@@ -1,11 +1,25 @@
 import pyautogui
 from PIL import ImageGrab
 import time
+from pynput import keyboard
 
+##### Helper functions #####
 def get_rgb_hex(rgb):
     # Take the first 3 values (R, G, B) to handle edge case where alpha channel is present
     r, g, b = rgb[:3]
     return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+
+def on_press(key):
+    if hasattr(key, 'char') and key.char == 's':
+        global hex_color
+        
+        with open('rgb_hex_colors.txt', 'a') as f:
+            f.write(f'{hex_color}\n')
+            print(f'Saved {hex_color} to rgb_hex_colors.txt')
+
+##### Main program #####
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
 print('Press Ctrl+C to stop program...')
 
@@ -21,7 +35,7 @@ try:
         hex_color = get_rgb_hex(rgb)
         
         print(f'RGB: {rgb} | HEX: {hex_color}        ', end='\r')
-
         time.sleep(0.1)
+            
 except KeyboardInterrupt:
     print("\nExiting...")
